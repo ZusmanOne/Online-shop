c803bc472673375d642c474c14c61aa4c5c45ee8
Nikita <bigmama93@mail.ru>
2021-11-01 17:29:18 +0300

    создано приложение для корзины товаров

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#создается ключ по которому бдут хранится данные корзины в сессии
+CART_SESSION_ID = 'cart'
