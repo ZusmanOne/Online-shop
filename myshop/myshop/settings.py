985916e411098a39a37e3cb283430a9d0c8cce94
Nikita <bigmama93@mail.ru>
2022-01-28 15:31:49 +0300

    установлена postgsql

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -99,10 +99,21 @@ WSGI_APPLICATION = 'myshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'online_shop',
+        'USER': 'myshop',
+        'PASSWORD' : 'myshop',
+        'HOST': 'localhost',
+        'PORT' : '5432'
     }
 }
 
